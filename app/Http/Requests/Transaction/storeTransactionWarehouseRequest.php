5e78de78e43565d7dfae2ae785a008f08385532f
Omqr25 <msqr2623@gmail.com>
2024-08-14 01:11:12 +0300

    TRANSACTION_TYPE v2.0

--- app/Http/Requests/Transaction/storeTransactionWarehouseRequest.php
+++ app/Http/Requests/Transaction/storeTransactionWarehouseRequest.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Requests\Transaction;
 
-use App\Enums\transactionModeType;
-use App\Enums\transactionType;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rules\Enum;
 
 class storeTransactionWarehouseRequest extends FormRequest
 {
@@ -27,8 +24,6 @@ class storeTransactionWarehouseRequest extends FormRequest
         return [
             'transaction_id' => 'required|exists:transactions,id',
             'warehouse_id' => 'required|exists:warehouses,id',
-            'transaction_type' => 'required',new Enum(transactionType::class),
-            'transaction_mode_type' => 'required',new Enum(transactionModeType::class),
             'quantity' => ['required', 'numeric', 'min:1'],
             'item_id'=>'required|integer'
         ];
--- app/Http/Resources/DonorTransactionResource.php
+++ app/Http/Resources/DonorTransactionResource.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Resources;
 
-
+use App\Enums\transactionModeType;
+use App\Enums\transactionType;
 use App\Enums\transactionStatusType;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -24,6 +25,8 @@ class DonorTransactionResource extends JsonResource
             'code' => $this->code,
             'status' => $this->status instanceof transactionStatusType ? $this->status->name : null,
             'date' => $this->date,
+            'transaction_type' => $this->transaction_type instanceof transactionType ? $this->transaction_type->name : null,
+            'transaction_mode_type' => $this->transaction_mode_type instanceof transactionModeType ? $this->transaction_mode_type->name : null,
             'waybill_num' => $this->waybill_num,
             'waybill_img' => $this->imageUrl('waybill_img'),
             'qr_code' => $this->imageUrl('qr_code'),
